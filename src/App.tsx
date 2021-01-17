accdcc2c8bc90682f0c5b22e1a0834bcf0b2936d
artyrkozel <artyr_42@mail.ru>
2021-01-17 06:55:28 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import styles from'./App.module.css';
+
+function App() {
+  return (
+    <div className={styles.App}>
+      <header className={styles.App}>
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
